eed46d082dfb709f843c64e9f1e7d80a5855cb02
akhil2502 <akhilachilies>
2021-09-15 17:24:01 +0530

    [akhil] Add:uc6

--- Employeewage.js
+++ Employeewage.js
@@ -3,10 +3,16 @@ const IS_fULL_tIME =2;
 const PART_TIME_HOURS =4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
+const IS_PART_TIME =1;
+const IS_fULL_tIME =2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOURS=20;
 const WORKING_DAYS=20;
 const MAX_HOURS=160;
 let empHrs=0;
 let workingDays=0
+let empDailyWageArr = new Array();
 function getWorkingHours(empCheck)
 {
     switch(empCheck)
@@ -19,12 +25,17 @@ function getWorkingHours(empCheck)
             return 0;
     }
 }
+function calcDailyWage(empHrs)
+{
+    return empHrs*WAGE_PER_HOURS;;
+}
 while(empHrs<=MAX_HOURS && workingDays <= WORKING_DAYS)
 {
     workingDays++;
     let empCheck = Math.floor(Math.random()*10)%3;
     empHrs += getWorkingHours(empCheck);
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
 let TotalWorkingDays=workingDays-1;
-let empWage= empHrs*WAGE_PER_HOURS;
+let empWage = calcDailyWage(empHrs);
 console.log("\nTotal Days : "+TotalWorkingDays+"\nTotal Hours : "+empHrs+"\nEmployee Wage: "+ empWage);
